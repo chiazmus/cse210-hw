0d03bf3f48fec08461d15a60f40252799e48ff34
chiazmus <andrew.burnah@gmail.com>
2025-05-13 11:15:11 +1200

    resume stuffy

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume myResume = new Resume();
+        myResume._name = "Andrew Burnah";
+
+        // I wish I actually had these jobs...
+        // Someday.
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
+
     }
 }
