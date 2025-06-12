3da930a0d0bb799591310ce3379b5e9309e75b59
chiazmus <andrew.burnah@gmail.com>
2025-06-13 10:40:36 +1200

    learning activity yay

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square(10, "red"));
+        shapes.Add(new Circle(5, "blue"));
+        shapes.Add(new Rectangle(4, 3, "green"));
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine($"Color: {shape.GetColor()}");
+        }
+
+
     }
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,17 @@
+class Rectangle : Shape
+{
+    private double _width;
+    private double _length;
+
+    public Rectangle(double width, double length, string color) : base(color)
+    {
+        _width = width;
+        _length = length;
+    }
+
+    public override double GetArea()
+    {
+        return _width * _length;
+    }
+
+}
